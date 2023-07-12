00b6c75150c30aa6d32135f0c65f216f02fb2063
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2023-07-12 14:48:56 +0300

    Initial Commit

---
+++ PereferenceKey/PereferenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PereferenceKeyApp.swift
+//  PereferenceKey
+//
+//  Created by Nour Gweda on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PereferenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
